9c389b979fdfe3659eff9449913fdda44325e5c0
Tjaos <83287716@gmail.com>
2025-05-25 16:06:12 -0300

    fix: fix error in build

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,11 +1,11 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2025-04-30.basil",
 });
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const body = await request.json();
   const { product, quantity } = body;
   if (!product || !quantity) {
