dac23329e11483c9f01b15c3cd138e925148a8c5
Tjaos <83287716@gmail.com>
2025-05-25 16:20:12 -0300

    chore: mudança na forma de receber o id

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,34 +1,37 @@
 import { db } from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function PUT(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+function extractIdFromPathName(pathName: string) {
+  const pathParts = pathName.split("/");
+  const id = pathParts[pathParts.length - 1];
+  return id;
+}
+
+export async function PUT(request: NextRequest) {
+  const id = extractIdFromPathName(request.nextUrl.pathname);
   const body = await request.json();
   const { name, description, price } = body;
 
   const product = await db.product.update({
-    where: { id: params.id },
+    where: { id },
     data: { name, description, price: parseFloat(price) },
   });
 
   return NextResponse.json(product);
 }
-export async function GET(_: Request, { params }: { params: { id: string } }) {
+export async function GET(request: NextRequest) {
+  const id = extractIdFromPathName(request.nextUrl.pathname);
   const product = await db.product.findUnique({
-    where: { id: params.id },
+    where: { id },
   });
 
   return NextResponse.json(product);
 }
 
-export async function DELETE(
-  _: Request,
-  { params }: { params: { id: string } }
-) {
+export async function DELETE(request: NextRequest) {
+  const id = extractIdFromPathName(request.nextUrl.pathname);
   await db.product.delete({
-    where: { id: params.id },
+    where: { id },
   });
 
   return NextResponse.json({ message: "produto deletado!" });
